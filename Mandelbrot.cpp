667c0a626582e03832d3e7e9d01621213435baca
stu124043 <stu124043@mail.uni-kiel.de>
2020-11-16 10:37:40 +0100

    created the repetition checker.

--- Mandelbrot.cpp
+++ Mandelbrot.cpp
@@ -14,6 +14,26 @@ void Mandelbrot::generateSet(int step) {
 }
 
 bool Mandelbrot::checkRepetition(double n) {
+	//check for false input
+	if (n > 2 || n < (-2))return false;
+
+	//init
 	bool result = false;
+	std::vector<double> z;
+	z.push_back(0 + n);
+
+	//algo
+	for (int i = 1; i < step; i++) {
+		double f = pow(z.back(), 2) + n;
+		for (double &e : z) {
+			if (e == f) {
+				result = true;
+				break;
+			}
+		}
+		z.push_back(f);
+	}
+
+	//return
 	return result;
 }
--- Mandelbrot.h
+++ Mandelbrot.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 class Mandelbrot {
 public:
